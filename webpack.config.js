5e4ee451260a9afa6c4fb3e5c9029f7ce1b12af9
Vlad Omelyanenko <shield.deffence@gmail.com>
2020-07-05 15:06:12 +0300

    ended config webpack

---
+++ webpack.config.js
@@ -0,0 +1,145 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin')
+const TerserWbpackPlugin = require('terser-webpack-plugin')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+
+const isDev = (process.env.NODE_ENV === "development");
+const isProd = !isDev
+
+const optimization = () => {
+  const config = {
+
+  }
+  if(isProd) {
+    config.minimizer = [
+      new OptimizeCssAssetsWebpackPlugin(),
+      new TerserWbpackPlugin()
+    ]
+  }
+  return config
+}
+const filename = (ext) => isDev ? `[name].${ext}` : `[name].[hash].${ext}`
+const cssLoaders = (extra) => {
+  const loaders = [
+    {
+      loader: MiniCssExtractPlugin.loader,
+      options: { 
+        hmr: isDev,
+        reloadall: true,
+      },
+    },
+    "css-loader",
+  ]
+  if (extra) {
+    loaders.push(extra)
+  }
+  return loaders
+}
+const babelOptions = (preset) => {
+  const options = {
+    presets: [
+      '@babel/preset-env'
+    ],
+    plugins: [
+      '@babel/plugin-proposal-class-properties'
+    ]
+  }
+  if (preset) {
+    options.presets.push(preset)
+  }
+  return options
+}
+
+const jsLoaders = () => {
+  const loaders = [
+    {
+      loader: 'babel-loader',
+      options: babelOptions()
+    }
+  ]
+  if(isDev) {
+    loaders.push('eslint-loader')
+  }
+  return loaders
+}
+const plugins = () => {
+  const base = [
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+      minify: {
+        collapseWhitespace: isProd
+      }
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: filename('css')
+    }),
+  ]
+  if (isProd) {
+    base.push(new BundleAnalyzerPlugin())
+  }
+  return base
+}
+
+
+module.exports = {
+  context: path.resolve(__dirname, "src"),
+  mode: "development",
+  entry: {
+    main: ["@babel/polyfill","./index.js"],
+  },
+  output: {
+    filename: filename('js'),
+    path: path.resolve(__dirname, "dist"),
+  },
+  resolve: {
+    extensions: [".js", ".json"],
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+  devServer: {
+    port: 9000,
+    hot: isDev
+  },
+  optimization: optimization(),
+  devtool: isDev ? 'source-map' : '',
+  plugins: plugins(),
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: cssLoaders()
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: cssLoaders('sass-loader')
+      },
+      {
+        test: /\.(png|jpg|svg|gif)$/,
+        use: ["file-loader"],
+      },
+      {
+        test: /\.(ttf|woff|woff2|eot)$/,
+        use: ["file-loader"],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: jsLoaders
+      },
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        loader: {
+          loader: "babel-loader",
+          options: babelOptions('@babel/preset-typescript')
+        }
+      }
+    ],
+  },
+};
